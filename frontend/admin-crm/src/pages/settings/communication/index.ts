e2f5861b8b12b80699a0891ba804fed03019c9ef
Stephen Deslate <stephendeslate@Stephens-MBP.local>
2025-06-22 19:18:04 -0700

    removed mock data and fixed file structure

--- frontend/admin-crm/src/pages/settings/communication/index.ts
+++
@@ -1,5 +0,0 @@
-// frontend/admin-crm/src/pages/settings/communication/index.ts
-
-export { CommunicationTemplates } from './CommunicationTemplates';
-export { CommunicationRecords } from './CommunicationRecords';
-export { CommunicationNotifications } from './Notifications';
