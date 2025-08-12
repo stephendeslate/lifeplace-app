92631a68a510eca82d025e94ad5e175a7f4f4de0
Stephen Deslate <stephendeslate@Mac.attlocal.net>
2025-08-12 13:43:46 -0700

    removed unused variables

--- frontend/admin-crm/src/pages/payments/PaymentsOverview.tsx
+++ frontend/admin-crm/src/pages/payments/PaymentsOverview.tsx
@@ -66,7 +66,6 @@ export const PaymentsOverview: React.FC = () => {
   const {
     payments = [],
     totalPayments,
-    pageCount,
     isLoadingPayments,
     createPayment,
     isCreatingPayment,
